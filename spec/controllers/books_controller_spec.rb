7176b515054d177b69ab5409d8b4571efde71432
Adam <adam.tecle@gmail.com>
2017-12-31 12:09:35 -0500

    Adds test that GET BookController#index returns all books

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe BooksController, type: :controller do
     it 'returns HTTP status 200' do
       expect(response).to have_http_status 200
     end
+
+    it 'returns all books' do
+      body = JSON.parse(response.body)
+      expect(body.size).to eq(10)
+    end
   end
 
 end
