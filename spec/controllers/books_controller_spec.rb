bff923632193dd41d14e3593fc3d383d5412339a
Adam <adam.tecle@gmail.com>
2017-12-31 12:04:31 -0500

    Adds test for GET BookController#index

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
-
   describe "GET #index" do
+    let!(:books) { FactoryBot.create_list(:book, 10) }
+
+    before { get :index }
+
+    it 'returns HTTP status 200' do
+      expect(response).to have_http_status 200
+    end
   end
 
 end
