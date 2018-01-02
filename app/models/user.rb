2684f6119f549a66faa894229d690f1c680b6615
Adam <adam.tecle@gmail.com>
2018-01-01 22:23:26 -0500

    Indexes User model by email and adds uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 
 end
