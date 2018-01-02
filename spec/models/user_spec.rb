5c2fc88d31d4073311d76e3795c34852335911aa
Adam <adam.tecle@gmail.com>
2018-01-01 22:02:03 -0500

    Adds tests in User model for email presence and uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has a valid factory" do
+    expect(FactoryBot.create(:user)).to be_valid
+  end
+
+  it "requires an email address" do
+    user = User.new
+    expect(user).to be_invalid
+  end
+
 end
