6b2e00e552062029b7e3ccf7f2665ebff8d4000f
Adam <adam.tecle@gmail.com>
2018-01-02 22:33:44 -0500

    Formatting

--- Gemfile
+++ Gemfile
@@ -10,14 +10,19 @@ ruby '2.4.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
+
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
+
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,35 +21,32 @@ RSpec.describe User, type: :model do
   it "should accept valid email addresses" do
     emails = %w[test@user.com user@user.com A-TEST-10@user.test.gov testing.T.T.e.st.test@company.com]
 
-      password = "password"
+    password = "password"
 
-      emails.each do |email|
-        user = User.create(email: email, password: password, password_confirmation: password)
-        expect(user).to be_valid
-      end
+    emails.each do |email|
+      user = User.create(email: email, password: password, password_confirmation: password)
+      expect(user).to be_valid
     end
+  end
 
-    it "should not accept invalid email addresses" do
-      emails = %w[testest.com user@user A-TEST-10@user.
-        testing.T.T.e.st.test@.test]
-        password = "password"
-        emails.each do |email|
-          user = User.create(email: email,
-            password: password,
-            password_confirmation: password)
-            expect(user).to be_invalid
-          end
-        end
+  it "should not accept invalid email addresses" do
+    emails = %w[testest.com user@user A-TEST-10@user. testing.T.T.e.st.test@.test]
+    password = "password"
+    emails.each do |email|
+      user = User.create(email: email, password: password, password_confirmation: password)
+      expect(user).to be_invalid
+    end
+  end
 
-        it "must have a unique email address" do
-          email = "test@test.com"
-          password = "password"
+  it "must have a unique email address" do
+    email = "test@test.com"
+    password = "password"
 
-          first_user = User.create(email: email, password: password, password_confirmation: password)
-          expect(first_user).to be_valid
+    first_user = User.create(email: email, password: password, password_confirmation: password)
+    expect(first_user).to be_valid
 
-          second_user = User.create(email: email, password: password, password_confirmation: password)
-          expect(second_user).to be_invalid
-        end
+    second_user = User.create(email: email, password: password, password_confirmation: password)
+    expect(second_user).to be_invalid
+  end
 
-      end
+end
